00634054e01293ee1bef68a00e015b1dfbc13525
Sahab Singh <akhil.nain.1430@gmail.com>
2021-07-03 23:35:28 +0530

    Second Version

---
+++ app/src/main/java/sahab/singh/digicollage/Home/Home_Activity_Teacher.java
@@ -0,0 +1,16 @@
+package sahab.singh.digicollage.Home;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import sahab.singh.digicollage.R;
+
+public class Home_Activity_Teacher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_teacher);
+    }
+}
---
+++ app/src/main/java/sahab/singh/digicollage/LoginActivity/LoginActivityAdmin.java
@@ -0,0 +1,16 @@
+package sahab.singh.digicollage.LoginActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import sahab.singh.digicollage.R;
+
+public class LoginActivityAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_student);
+    }
+}
